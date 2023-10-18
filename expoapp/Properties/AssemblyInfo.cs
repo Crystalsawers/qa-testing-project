7e65f389aadc009e172cd635fb5c59f14621545b
Crystal Sawers <sawece1@student.op.ac.nz>
2023-10-19 12:39:22 +1300

    update

---
+++ expoapp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("expoapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Otago Polytechnic")]
+[assembly: AssemblyProduct("expoapp")]
+[assembly: AssemblyCopyright("Copyright © Otago Polytechnic 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f2f8b8c1-9328-4668-a1f1-711e6a7575b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
